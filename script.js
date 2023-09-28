85f146ed84324df90be9a8c7e93b0eaa7c378af4
AbdurRahman <arshadhinjr@gmail.com>
2023-09-28 19:24:54 +0600

    my first gsap project

---
+++ script.js
@@ -0,0 +1,35 @@
+let tl = gsap.timeline()
+gsap.from( "#nav img , #nav li , #nav button" , {
+    duration: .3,
+    y: -50, 
+    opacity: 0, 
+    stagger: 0.1
+} )
+tl.from( "#hero h1", {
+    duration: .4,
+    y: 50,
+    opacity: 0, 
+    stagger: 0.4
+} )
+gsap.from( "#hero span", {
+    duration: 1,
+    opacity:0.5, 
+    delay: 0.7, 
+    repeat: -1,
+    yoyo: true
+} )
+gsap.from( ".img img", {
+    duration: 1,
+    opacity: 0,
+    scale: 0.5, 
+    stagger: 0.2, 
+    delay: .2
+})
+
+gsap.to( "#scroll", {
+    duration: 1,
+    y: 10, 
+    repeat: -1,
+    yoyo: true
+} )
+
